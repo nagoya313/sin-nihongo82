bf96bdd4ab5ce5e53c78a1270f44b6878f15d750
nagoya313 <nagoya313@gmail.com>
2022-03-27 03:31:16 +0900

    version up

--- src/components/molecules/PageTitle.client.tsx
+++ src/components/molecules/PageTitle.client.tsx
@@ -3,17 +3,13 @@ type PageTitleProps = {
   title: string;
 };
 
-const PageTitle = ({ avatar, title }: PageTitleProps) => {
-  console.log(avatar);
-
-  return (
-    <div className="flex flex-row items-center">
-      <div className="avatar placeholder">
-        <div className="rounded-full w-12 h-12 text-white bg-accent">{avatar}</div>
-      </div>
-      <h1 className="ml-4 mb-0">{title}</h1>
+const PageTitle = ({ avatar, title }: PageTitleProps) => (
+  <div className="flex flex-row items-center">
+    <div className="avatar placeholder">
+      <div className="rounded-full w-12 h-12 text-white bg-accent">{avatar}</div>
     </div>
-  );
-};
+    <h1 className="ml-4 mb-0">{title}</h1>
+  </div>
+);
 
 export default PageTitle;
