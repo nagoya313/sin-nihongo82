2c7c1645f78f8f870a04269926577031b7b85462
nagoya313 <nagoya313@gmail.com>
2022-04-10 16:23:28 +0900

    ソート順タブ追加

--- src/components/organisms/RadicalSearch.client.tsx
+++ src/components/organisms/RadicalSearch.client.tsx
@@ -1,4 +1,4 @@
-import { HStack, VStack } from '@chakra-ui/react';
+import { HStack, Tab, TabList, TabPanel, TabPanels, Tabs, VStack } from '@chakra-ui/react';
 import SearchPanel from '../../components/molecules/SearchPanel.client';
 import { radicalQueryParams } from '../../features/radical/queryParams';
 import { useSearch } from '../../libs/hooks/useSearch';
@@ -11,19 +11,39 @@ const RadicalSearch = () => {
   const { min, max } = getNumberRange(radicalQueryParams.shape.strokeCount);
 
   return (
-    <SearchPanel>
-      <VStack as="form" w="full" align="start">
-        <HStack align="start">
-          <StringInputField
-            label="よみかた"
-            help="部首名は新日本語表音式によるひらがなでの前方一致で絞り込みができます。"
-            {...register('read', true)}
-            placeholder="いち、しょー、つずみ"
-          />
-          <NumberInputField label="画数" {...register('strokeCount', true)} placeholder={`${min}〜${max}`} />
-        </HStack>
-      </VStack>
-    </SearchPanel>
+    <>
+      <SearchPanel>
+        <VStack as="form" w="full" align="start">
+          <HStack align="start">
+            <StringInputField
+              label="よみかた"
+              help="部首名は新日本語表音式によるひらがなでの前方一致で絞り込みができます。"
+              {...register('read', true)}
+              placeholder="いち、しょー、つずみ"
+            />
+            <NumberInputField label="画数" {...register('strokeCount', true)} placeholder={`${min}〜${max}`} />
+          </HStack>
+        </VStack>
+      </SearchPanel>
+      <Tabs
+        mt={4}
+        colorScheme="purple"
+        onChange={(index) => register('sort').onChange(index === 0 ? 'stroke_count' : 'read')}
+      >
+        <TabList>
+          <Tab>画数順</Tab>
+          <Tab>よみかた順</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <p>画数順！</p>
+          </TabPanel>
+          <TabPanel>
+            <p>讀み方順！</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </>
   );
 };
 
