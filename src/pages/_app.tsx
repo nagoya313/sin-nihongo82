7b4933243c288dd522dc6633120313f63fdd7e29
nagoya313 <nagoya313@gmail.com>
2022-04-10 12:38:34 +0900

    devでのhydration error對策

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,11 @@
 import type { AppProps } from 'next/app';
+import Layout from '../components/organisms/Layout.client';
 
-const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+// Layoutで括らないとHydrate errorが出る模樣
+const App = ({ Component, pageProps }: AppProps) => (
+  <Layout>
+    <Component {...pageProps} />
+  </Layout>
+);
 
 export default App;
