cbbdb8da89f17bce18a2096f57ddc443c3ec0834
nagoya313 <nagoya313@gmail.com>
2022-04-23 12:41:50 +0900

    pathを型安全化

--- src/features/path.ts
+++ src/features/path.ts
@@ -1,8 +1,17 @@
+import { ValueOf } from 'type-fest';
+
 export const Path = {
   info: '/info',
   radicals: '/radicals',
   radical: (codePoint: number) => `/radicals/${codePoint}` as const,
-  radicalEdit: (codePoint: number) => `/radicals/${codePoint}/edit`,
+  radicalEdit: (codePoint: number) => `/radicals/${codePoint}/edit` as const,
 } as const;
 
-export type PathString = string;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type PathValue<TValue extends ValueOf<typeof Path>> = TValue extends (...params: any[]) => string
+  ? ReturnType<TValue>
+  : TValue;
+
+export type PathString = ValueOf<{
+  [key in keyof typeof Path]: PathValue<typeof Path[key]>;
+}>;
